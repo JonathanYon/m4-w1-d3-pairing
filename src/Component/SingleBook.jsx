aa2c03f838ed74e94a5bf73fcb799e2eb1b8881a
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-07-07 18:31:00 +0200

    small correction

--- src/Component/SingleBook.jsx
+++ src/Component/SingleBook.jsx
@@ -1,11 +1,12 @@
-import { Card, Button } from "react-bootstrap"
+import { Card, Button, Col} from "react-bootstrap"
 import React from "react"
 
 export default class SingleBook extends React.Component {
     render(){
 
         const {book} = this.props;
-        return <Card style={{ width: '18rem' }}>
+        return <Col sm={12} md={3} className="d-flex mb-3 " key={book.asin}>  
+        <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src={book.img} />
         <Card.Body>
           <Card.Title>{book.title}</Card.Title>
@@ -15,6 +16,7 @@ export default class SingleBook extends React.Component {
           <Button variant="primary" className="px-5">{book.price}</Button>
         </Card.Body>
       </Card>
+      </Col>
     }
 }
 
