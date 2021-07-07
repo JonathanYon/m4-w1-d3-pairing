8a7b2da2e1be8108fbe78388b7b045d663d0effb
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-07-07 18:05:14 +0200

    with class

--- src/Component/BookList.jsx
+++ src/Component/BookList.jsx
@@ -1,33 +1,24 @@
 import { Container, Row, Col, Card, Button, InputGroup, FormControl } from "react-bootstrap"
+import SingleBook from "./SingleBook"
 
 const BookList = ({books}) => (
+
+    
     
       <Container className="mt-5">
-          <InputGroup className="mb-3">
-            <InputGroup.Text>Search Here</InputGroup.Text>
-            <FormControl aria-label="Search" />
-          </InputGroup>
+          
           <Row>
              
               {
-                  books.map(ele => (
-                    <Col sm={12} md={3} className="d-flex mb-3 " key={ele.asin}>  
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src={ele.img} />
-                            <Card.Body>
-                            <Card.Title>{ele.title}</Card.Title>
-                            <Card.Text>
-                                {ele.category}
-                            </Card.Text>
-                            <Button variant="primary" className="px-5">{ele.price}</Button>
-                        </Card.Body>
-                    </Card>
-                  </Col>
+                  books.map(book => (
+                    <SingleBook key={book.asin} book={book}/>
                   )).slice(0, 16)
               }
               
           </Row>
-      </Container>  
+      </Container> 
+
+    
    
 )
 export default BookList
--- src/Component/SingleBook.jsx
+++ src/Component/SingleBook.jsx
@@ -1,16 +1,20 @@
 import { Card, Button } from "react-bootstrap"
+import React from "react"
 
-const SingleBook = ({book}) => (
-   
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={book.img} />
-  <Card.Body>
-    <Card.Title>{book.title}</Card.Title>
-    <Card.Text>
-      {book.category}
-    </Card.Text>
-    <Button variant="primary" className="px-5">{book.price}</Button>
-  </Card.Body>
-</Card>
-)
-export default SingleBook
+export default class SingleBook extends React.Component {
+    render(){
+
+        const {book} = this.props;
+        return <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={book.img} />
+        <Card.Body>
+          <Card.Title>{book.title}</Card.Title>
+          <Card.Text>
+            {book.category}
+          </Card.Text>
+          <Button variant="primary" className="px-5">{book.price}</Button>
+        </Card.Body>
+      </Card>
+    }
+}
+
